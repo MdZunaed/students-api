1a94b9fbd288a73c2cd7d79f3e58dcbd532ccd6b
Zunayed <mdzunaed4@gmail.com>
2025-07-27 16:52:16 +0600

    graceful shutdown

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -1,7 +1,68 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/MdZunaed/students-api/internal/config"
+)
 
 func main() {
-	fmt.Println("Welcome to students api")
+	// Load config
+	cfg := config.MustLoad()
+
+	// Database setup
+
+	// Setup router
+
+	router := http.NewServeMux()
+
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome to Students api"))
+	})
+	router.HandleFunc("GET /test", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Test page"))
+	})
+
+	// Setup server
+
+	server := http.Server{
+		Addr:    cfg.Addr,
+		Handler: router,
+	}
+	slog.Info("Server started", slog.String("address", cfg.Addr))
+
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil {
+			log.Fatal("Failed to start server")
+		}
+	}()
+
+	<-done
+
+	/// Graceful shutdown: If any issue with server,
+	/// Server will shutdown after the current request. no instantly
+
+	slog.Info("shutting down the server")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	// err:= server.Shutdown(ctx)
+	// if err != nil {
+	// 	slog.Error("Failed to shutdown server", slog.String("error", err.Error()))
+	// }
+	// Shortcut:
+	if err := server.Shutdown(ctx); err != nil {
+		slog.Error("Failed to shutdown server", slog.String("error", err.Error()))
+	}
+	slog.Info("Server shutdown successfully")
 }
